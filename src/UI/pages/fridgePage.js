b37bdad39a0f04ac035dd9b883cfb662c5c12bda
4l3x4ndre <alexandre@amrani.fr>
2020-12-20 15:10:14 +0100

    FETCH - basics fetch and first constant

--- src/UI/pages/fridgePage.js
+++ src/UI/pages/fridgePage.js
@@ -3,17 +3,27 @@ import React from 'react'
 import Searchbar from '../searchbar/Searchbar'
 import Recipes from '../../recipe/Recipes'
 
+import constants from '../../API/constants.js'
+
+
 class FridgePage extends React.Component {
 
     /**
      * This Component acts like the link between the SearchBar and the Recipes.
      * It passes the list of food elements from the SearchBar to  the Recipes.
+     * 
+     * The state.data is the data from the API
+     * 
      */
 
     constructor(props) {
         super(props)
         this.state = {
-            listFoodstuffs: null
+            listFoodstuffs: null,
+            baseUrl: 'https://api.spoonacular.com/recipes/',
+            data: null,
+            fetchError: null,
+            fetchLoading: false
         }
     }
 
@@ -28,7 +38,33 @@ class FridgePage extends React.Component {
             listFoodstuffs : newlist
         }, () => {
             // fetch data
+            this.fetchData('complexSearch', '&query=pasta&maxFat=25&number=2')
         })
+    }
+
+    fetchData(search, query) {
+        if (this.state.fetchLoading) return 
+
+        this.setState({
+            fetchLoading: true
+        })
+
+        let url = this.state.baseUrl + search + '?apiKey=' + constants.APIKEY + query
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    data: data,
+                    fetchLoading: false
+                }, () => console.log(this.state.data))
+            })
+            .catch((e) => {
+                this.setState({
+                    fetchError: e,
+                    fetchLoading: false
+                })
+            })
     }
 
     componentDidMount() {
